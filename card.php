f68954f69fe4a1a31170617972d3cd835a25dd11
Pierre Dumont <pierre.dmt59@gmail.com>
2018-09-10 22:22:17 +0200

    include header in file index.php

---
+++ card.php
@@ -0,0 +1,25 @@
+<?php
+
+$cardTab = [
+[
+  'marque' => 'Converse',
+  'prix' => '45€',
+],
+[
+  'marque' => 'Vans',
+  'prix' => '60€',
+],
+[
+  'marque' => 'Nike',
+  'prix' => '80€',
+],
+[
+  'marque' => 'Adidas',
+  'prix' => '50€',
+]
+];
+
+foreach ($cardTab as $value) {
+  echo $value;
+}
+ ?>
---
+++ header.php
@@ -0,0 +1,34 @@
+<header>
+  <nav class="col-12 col-sm-12 col-md-12 d-lg-none  navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="index.php"><img class="logo" src="img/logo.png" alt="logo"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Homme</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Femme</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Fille</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Garçon</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <a href="index.php"><img class="d-none d-sm-none d-md-none d-lg-block logo" src="img/logo.png" alt="logo"></a>
+  <div class= "d-sm-none d-md-none d-lg-block col-lg-12 menu">
+    <ul class="d-none d-sm-none d-md-none d-lg-block">
+      <li><a class="test" href="#">Homme </a></li>
+      <li><a class="test" href="#">Femme </a></li>
+      <li><a class="test" href="#">Fille </a></li>
+      <li><a class="test" href="#">Garçon </a></li>
+    </ul>
+  </div>
+<header>
